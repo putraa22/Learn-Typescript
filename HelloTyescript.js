7ede77b3071621c8458a031e8e7f9405f5c6804b
Hermansyah Putra <hermansyahputra0418@gmail.com>
2022-10-29 01:18:36 +0700

    Hello World

---
+++ HelloTyescript.js
@@ -0,0 +1,6 @@
+function sayHello(name) {
+    return "Hello ".concat(name);
+}
+;
+var response = sayHello("Ramy");
+console.log(response);
---
+++ HelloTyescript.ts
@@ -0,0 +1,6 @@
+function sayHello(name: string) {
+    return `Hello ${name}`
+};
+let response : string = sayHello("Ramy");
+
+console.log(response);
